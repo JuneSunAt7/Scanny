7738048ee0c7760282722d1965af5fd7dc21fac1
JuneSunAt7 <jprogram@bk.ru>
2026-09-12 18:35:42 +0300

    create generator of syn packets

--- src/scan/syn_packet.go
+++ src/scan/syn_packet.go
@@ -6,14 +6,11 @@ import (
 	"://github.com"
 	"://github.com/layers"
 )
-
-// sendSYNPacket собирает и отправляет один TCP SYN пакет
 func SendSYNPacket(srcIP, dstIP string, srcPort, dstPort int) error {
-	// 1. Преобразуем IP-адреса в формат net.IP
 	srcIPNet := net.ParseIP(srcIP)
 	dstIPNet := net.ParseIP(dstIP)
 
-	// 2. Создаем слой IPv4
+	// 2. create layer IPv4
 	ipLayer := &layers.IPv4{
 		SrcIP:    srcIPNet,
 		DstIP:    dstIPNet,
@@ -22,44 +19,34 @@ func SendSYNPacket(srcIP, dstIP string, srcPort, dstPort int) error {
 		Protocol: layers.IPProtocolTCP,
 	}
 
-	// 3. Создаем слой TCP с флагом SYN
+	// 3. create layer TCP flag SYN
 	tcpLayer := &layers.TCP{
 		SrcPort: layers.TCPPort(srcPort),
 		DstPort: layers.TCPPort(dstPort),
-		Seq:     110502, // Произвольное стартовое число (Sequence Number)
-		SYN:     true,   // Включаем флаг SYN
-		Window:  14600,  // Размер окна передачи
+		Seq:     110502, // random number
+		SYN:     true,   
+		Window:  14600,  
 	}
-
-	// Критически важно: настраиваем расчет контрольной суммы TCP.
-	// TCP требует "псевдо-заголовок" IP для валидации чексумы.
 	if err := tcpLayer.SetNetworkLayerForChecksum(ipLayer); err != nil {
 		return err
 	}
 
-	// 4. Буферизируем и сериализуем пакет в байты
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{
-		ComputeChecksums: true, // Авторасчет контрольных сумм IP и TCP
+		ComputeChecksums: true, 
 		FixLengths:       true,
 	}
 
-	// Собираем слои вместе
 	err := gopacket.SerializeLayers(buf, opts, ipLayer, tcpLayer)
 	if err != nil {
 		return err
 	}
-
-	// 5. Открываем сырой сокет (Raw Socket) для отправки IPv4/TCP
-	// "ip4:tcp" означает, что ОС сама добавит Ethernet-заголовок, 
-	// но уровни IP и TCP мы контролируем полностью.
 	conn, err := net.Dial("ip4:tcp", dstIP)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	// 6. Отправляем байты в сеть
 	_, err = conn.Write(buf.Bytes())
 	return err
 }
